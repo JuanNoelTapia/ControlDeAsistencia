8cb97be1c38095c88befe038e6942f7e88fa949a
JuanNoelTapia <jtapiablandon@gmail.com>
2024-10-06 09:17:36 -0600

    Primer cambio de la version

---
+++ ControlDeAsistencia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ControlDeAsistencia
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Universidad.Estudiante> estudiantes = new List<Universidad.Estudiante>
+        {
+            new Universidad.Estudiante("Juan Tapia", 27, 27),
+            new Universidad.Estudiante("Daniela Castillo", 27, 16),
+            new Universidad.Estudiante("Sara Blandon", 27, 24),
+            new Universidad.Estudiante("Erlan Surez", 27, 25),
+        };
+
+        foreach (var estudiante in estudiantes)
+        {
+            estudiante.MostrarInformacion();
+        }
+
+        }
+    }
+
+}
---
+++ ControlDeAsistencia/Universidad.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ControlDeAsistencia
+{
+    public class Universidad //clase contenedora
+    {
+        public class Estudiante//clase anidada
+        {
+            public Estudiante(string nombreEstudiante, int sesionesTotales, int sesionesAsistidas)
+            {
+                NombreEstudiante = nombreEstudiante;
+                SesionesTotales = sesionesTotales;
+                SesionesAsistidas = sesionesAsistidas;
+            }
+
+            public string NombreEstudiante { get; set; }
+            public int SesionesTotales { get; set; }
+            public int SesionesAsistidas { get; set; }
+
+            internal class Asistencia
+            {
+                public static double CalcularPorcentaje(Estudiante estudiante)
+                {
+                    return (double)estudiante.SesionesAsistidas / estudiante.SesionesTotales * 100;
+                }
+
+                public static bool MinimoAsistenciasCumplidas(Estudiante estudiante, double porcentajeMinimo = 75)
+                {
+                    double porcentaje = CalcularPorcentaje(estudiante);
+                    return porcentaje >= porcentajeMinimo;
+                }
+            }
+            public void MostrarInformacion()
+            {
+                double porcentaje = Asistencia.CalcularPorcentaje(this);
+                bool minimoAsistenciasCumplidas = Asistencia.MinimoAsistenciasCumplidas(this);
+
+                Console.WriteLine($"Estudiante: {NombreEstudiante}");
+                Console.WriteLine($"Porcentaje de asistencia: {porcentaje:F2}%");
+                if (minimoAsistenciasCumplidas)
+                {
+                    Console.WriteLine("Cumple con el requisito de asistencia.");
+                }
+                else
+                {
+                    Console.WriteLine("No cumple con el requisito de asistencia.");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+
+    }
+}
